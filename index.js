67bf12c4ff8e45b72c4dfc5efc50a1220900f186
eric <eric.della@yahoo.com>
2020-08-25 18:52:39 -0700

    finished tasks, no stretch

--- index.js
+++ index.js
@@ -118,24 +118,10 @@ Hint: You can use .splice() for this
 
 */
 
-// function removeFlavorByName(a, b) {
-//   var i = 0;
-//   while (i < a.length) {
-//     if (a[i] === b) {
-//       a.splice(i, 1);
-//     } else {
-//       i++;
-//     }
-//   }
-//   return a;
-// }
-
 function removeFlavorByName(a, b) {
   for (i = 0; i <= a.length; i++) {
     if (a[i] === b) {
       a.splice(i, 1);
-    } else {
-      i++;
     }
   }
   return a;
@@ -151,8 +137,9 @@ Your function should accept:
 
 and should return a new array that is identical to the old array. You can name the new array however you'd like. */
 
-function copy(/*code here*/) {
-  /*code here*/
+function copy(oldArray, newArray) {
+  newArray = [...oldArray];
+  return newArray;
 }
 
 /* Task 7: July 7th is "World Chocolate Day" and Baskin Robins wants to create promotional materials highlighting all of their chocolate flavors. Write a function that checks every item in the array for a given string and returns a new array called filteredArray with just these values. Rather than hardcoding "chocolate" into your function, pass a string as a parameter, and invoke with the argument "chocolate". This way you could also filter for "Vanilla", "Sherbert", etc. when those holidays roll around.
@@ -170,9 +157,20 @@ DO NOT USE ADVANCED ARRAY METHODS (i.e. .filter) to solve this problem.
 
 hint - you can use the .includes method to help you solve this */
 
-function filterByWord(/*code here*/) {
-  /*code here*/
+function filterByWord(a, b) {
+  let filteredArray = [];
+  for (let i = 0; i < a.length; i++) {
+    if (a[i].includes(b)) {
+      filteredArray.push(a[i]);
+    }
+  }
+  return filteredArray;
 }
+
+console.log(filterByWord(originalFlavors, "Chocolate"));
+
+// var found = originalFlavors[6].includes("Chocolate");
+// console.log(found);
 
 /* 🧁🍦🍨 STRETCH 🍨🍦🍫*/
 
